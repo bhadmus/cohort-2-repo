e6da766bc67b39e247ba8c796d48f9a7a8be12cb
Ademola Bhadmus <sanguine@MacBookPro.lan>
2025-01-16 15:53:23 +0000

    trying parallel

--- gitlab-reporting/cypress.config.js
+++ gitlab-reporting/cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const cypressSplit = require('cypress-split')
 
 async function setupNodeEvents(on, config) {
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
   on('file:preprocessor', browserify.default(config));
+  cypressSplit(on, config)
 
   return config
 
